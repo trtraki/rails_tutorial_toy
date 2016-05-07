6de7cc52c370e1fd6e1047be079c2483ac20ed9b
trtraki <akio55252000@yahoo.co.jp>
2016-05-07 14:41:26 +0900

    micropostsを実装。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507045414) do
+ActiveRecord::Schema.define(version: 20160507051530) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "context"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
